39e501bc1df7d39869691b3f7a590d6bf6fec727
Guglielmo Marra <156308467+marraguglielmo@users.noreply.github.com>
2024-02-28 16:13:54 +0100

    [add] css e stampa

--- JS/script.js
+++ JS/script.js
@@ -1,12 +1,22 @@
+const outputPali = document.querySelector('.output-palindroma');
 const word = document.getElementById('word-input').value;
 // uso la funzione
 const wordReverse = reverse(word);
+// verifico se la parola è palindroma
+if(word == wordReverse){
+    outputPali.innerHTML = `La parola <strong>${word}</strong> è palindroma`;
+}else{
+    outputPali.innerHTML = `La parola <strong>${word}</strong> non è palindroma`;
+}
+
 
 
 // FUNZIONE
 function reverse(txt){
-    let txtReverse = txt.split('').reverse('');
+    // aggiungo il metodo join per riunire le lettere e poter confrontare le stringhe
+    let txtReverse = txt.split('').reverse('').join('');
     return txtReverse;
 }
 
+console.log(word);
 console.log(wordReverse);
