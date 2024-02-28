ac3d3394a8802a0f6494981f658109a6eeb41491
Guglielmo Marra <156308467+marraguglielmo@users.noreply.github.com>
2024-02-28 16:30:07 +0100

    [fix] random function

--- JS/script.js
+++ JS/script.js
@@ -23,12 +23,16 @@ function reverse(txt){
 const choicePlayer = 'pari'; // inserire input 
 const numPlayer = 2 //inserire input
 const choicePc = numRandomPc(1, 5);
+
 console.log(choicePc);
 
-
-
-
+/**
+ * funzione per estrarre un numero random da 1 a 5
+ * @param {number} min 
+ * @param {number} max 
+ * @returns number
+ */
 // FUNCTIONS
 function numRandomPc(min, max){
-    return Math.floor(Math.random() * (5 - 1 + 1) +1);
+    return Math.floor(Math.random() * (max - min + 1) + min);
 }
