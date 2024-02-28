e5c1768a1948f1a314d55daa1a585e0e86972e59
Guglielmo Marra <156308467+marraguglielmo@users.noreply.github.com>
2024-02-28 16:05:49 +0100

    [add] comment

--- JS/script.js
+++ JS/script.js
@@ -1,9 +1,9 @@
 const word = document.getElementById('word-input').value;
+// uso la funzione
 const wordReverse = reverse(word);
 
 
-
-
+// FUNZIONE
 function reverse(txt){
     let txtReverse = txt.split('').reverse('');
     return txtReverse;
